632721dfabf155f03bf888b949de755aee113110
ghyarhmoussa <ghyathmoussa@hotmail.com>
2020-09-15 22:54:07 +0300

    adding css, js files

---
+++ js/app.js
@@ -0,0 +1,48 @@
+// const url = require('url');
+
+// const path = require('path');
+// const { resolve } = require('path');
+
+// let result = path.resolve('app.js');
+// console.log(result);
+
+// const address = 'https://console.firebase.google.com/project/todolist-10467/overview';
+
+// result = url.parse(address,true);
+
+// console.log(result);
+
+// const fs = require('fs');
+
+// const fies = fs.readdir('./',function(error,data){
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log(data);
+//     }
+
+// });
+
+
+// fs.writeFile('test.txt','HelloWorld',function(err,data){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log('file created');
+//     }
+// })
+
+// Events Sections
+
+// const Event = require('events');
+
+// const emmiter = new Event();
+
+// const logger = require('./logger');
+
+// emmiter.on('connection',function(){
+//     console.log('connected');
+// });
+
+// logger('hello ghyath');
+
